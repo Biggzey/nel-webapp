397f688cbc4a434da068f1bb3e3b8fbfd2b27a52
Your Name <your.email@example.com>
2025-05-06 00:35:04 +0100

    chore: remove unused fix-roles files

--- prisma/migrations/add_user_roles.sql
+++
@@ -1,5 +0,0 @@
--- Add role column to User table
-ALTER TABLE "User" ADD COLUMN "role" TEXT NOT NULL DEFAULT 'member';
-
--- Set Biggzey as superadmin
-UPDATE "User" SET role = 'superadmin' WHERE username = 'Biggzey'; 
